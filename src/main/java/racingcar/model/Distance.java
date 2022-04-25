8807e51fd39773f3f109bf56f119f8ed5d67f833
Shin seongsu <tlstjdtn321@naver.com>
2022-04-25 15:44:21 +0900

    feat: 0-9 랜덤값을 구한 후 Go,Stop 표현하기

---
+++ src/main/java/racingcar/model/Distance.java
@@ -0,0 +1,24 @@
+package racingcar.model;
+
+public class Distance {
+    private static final String HYPHEN = "-";
+
+    private StringBuilder distance;
+
+    public Distance() {
+        this.distance = new StringBuilder();
+    }
+
+    public void increaseDistance() {
+        this.distance.append(HYPHEN);
+    }
+
+    public String getPosition() {
+        return distance.toString();
+    }
+
+    public int getDistence() {
+        return distance.length();
+    }
+
+}
